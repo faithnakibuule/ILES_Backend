596c7182717c50d398ec1dc8440b965b4f008579
Rahim Stone <tambwerahimstone@gmail.com>
2026-02-28 02:24:42 +0300

    fix: recreate Django project with correct structure using startproject config . and add proper .gitignore

--- config/wsgi.py
+++
@@ -1,9 +0,0 @@
-import os
-
-from django.core.wsgi import get_wsgi_application
-
-# This points Django to your settings file
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
-
-# This is the specific attribute the error was looking for!
-application = get_wsgi_application()
