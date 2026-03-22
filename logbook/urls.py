a29f4d7d474c97a2de8a813affc01e67df0b541e
Kakyubya Misha <mishasarah198v@gmail.com>
2026-03-22 22:48:46 +0300

    fixed both project and app  urls.py

--- logbook/urls.py
+++ logbook/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import WeeklyLogListView
+from users.views import WeeklyLogListView
+
 
 urlpatterns = [
     path('logs/', WeeklyLogListView.as_view(), name='weekly-logs')
--- urls.py
+++ urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import WeeklyLogListView
+from users.views import WeeklyLogListView
 
-urlpattersns = [
+urlpatterns = [
     path('logs/', WeeklyLogListView.as_view(), name='weekly-logs'),
 ]
