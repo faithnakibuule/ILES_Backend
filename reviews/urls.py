66aca67384336aea1571760cf39e22fa5da18bcd
Rahim Stone <tambwerahimstone@gmail.com>
2026-04-21 23:03:18 +0300

    Add api/reviews/criteria endpoint for evaluation viewset

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import NotificationViewSet, EvaluationViewSet, ReviewHistoryView
+from .views import NotificationViewSet, EvaluationViewSet, ReviewHistoryView, CriteriaViewSet
 
 router = DefaultRouter()
 router.register(r'notifications', NotificationViewSet, basename='notification')
 router.register(r'evaluations', EvaluationViewSet, basename='evaluation')
+router.register(r'criteria', CriteriaViewSet, basename='criteria')
 
 urlpatterns = [
-    path('',include(router.urls)),
-         path('logs/<int:log_id>/history/', ReviewHistoryView.as_view(), name = 'log-history'),
+    path('', include(router.urls)),
+    path('logs/<int:log_id>/history/', ReviewHistoryView.as_view(), name='log-history'),
 ]
